fb254366d637ddb54972cdacc690cc08e3273629
Jean-Luc Tritsch <jeanluc.tritsch@jlt-devconsulting.fr>
2023-09-26 14:18:29 +0200

    Solution exercice 2.2

--- FormationAlgo/Program.cs
+++ FormationAlgo/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// Demander à l'utilisateur de saisir un nombre
+Console.WriteLine("Veuillez saisir un nombre : ");
+
+// Recueillir le nombre saisi
+string saisie = Console.ReadLine();
+
+// Calculer le carré du nombre saisi
+// résultat = saisie * saisie;
+int nombre = int.Parse(saisie);
+int resultat = nombre * nombre;
+
+// Afficher le résultat
+Console.WriteLine($"Le carré de {nombre} est {resultat}");
