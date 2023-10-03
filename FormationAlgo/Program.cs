0b2a40f563289e032426fd73b410a097f0c213a4
Jean-Luc Tritsch <jeanluc.tritsch@jlt-devconsulting.fr>
2023-10-03 10:20:23 +0200

    Solution exercice 3.1

--- FormationAlgo/Program.cs
+++ FormationAlgo/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// Récupération du nombre
+Console.WriteLine("Veuillez saisir un nombre pour savoir s'il est positif ou négatif: ");
+string saisie = Console.ReadLine();
+
+// Conversion de string vers decimal
+decimal nombre = decimal.Parse(saisie);
+
+// Affichage conditionnel
+if (nombre > 0)
+{
+    Console.WriteLine("Le nombre est positif");
+}
+else if (nombre < 0)
+{
+    Console.WriteLine("Le nombre est négatif");
+}
+else
+{
+    Console.WriteLine("Le nombre est positif ET négatif (c'est zéro)");
+}
