3db61aa6116f36205ef4b15cd4954bd0d9790179
Jean-Luc Tritsch <jeanluc.tritsch@jlt-devconsulting.fr>
2023-10-10 09:58:58 +0200

    Solution Exercice 3.4

--- FormationAlgo/Program.cs
+++ FormationAlgo/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿// Demande de saisie du nombre
+Console.WriteLine("Veuillez saisir un nombre:");
+string saisieNombre = Console.ReadLine();
+int nombre = int.Parse(saisieNombre);
+
+if (nombre > 0)
+{
+    // Si le nombre est positif
+    Console.WriteLine("Le nombre est positif");
+}
+else if (nombre < 0)
+{
+    // Si le nombre est négatif
+    Console.WriteLine("Le nombre est négatif");
+}
+else
+{
+    // Autres cas
+    Console.WriteLine("Le nombre est zéro. Il est négatif ET positif.");
+}
+
+
+
+
+
+
+
