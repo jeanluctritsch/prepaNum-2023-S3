46efe68bebda0065ff9ba7483f61683dc5b3f9b0
Jean-Luc Tritsch <jeanluc.tritsch@jlt-devconsulting.fr>
2023-11-21 10:28:28 +0100

    Solution exercice 4.1

--- FormationAlgo/Program.cs
+++ FormationAlgo/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿// Saisie nombre compris entre 1 et 3
+Console.WriteLine("Veuillez saisir un nombre entre 1 et 3 (compris) :");
+string saisie = Console.ReadLine();
+
+int nombre = int.Parse(saisie);
+
+bool saisieValide = nombre >= 1 && nombre <= 3;
+bool saisieInvalide = !saisieValide;
+
+// Contrôle de la saisie
+while (saisieInvalide)
+{
+    Console.WriteLine("Ce nombre n'est pas compris entre 1 et 3 (compris).");
+    Console.WriteLine("Veuillez saisir un nombre entre 1 et 3 (compris) :");
+    saisie = Console.ReadLine();
+
+    nombre = int.Parse(saisie);
+    saisieValide = nombre >= 1 && nombre <= 3;
+    saisieInvalide = !saisieValide;
+}
+
+// Message si réponse OK
+Console.WriteLine($"Bravo ! Vous avez saisi {nombre} !");
+
+
