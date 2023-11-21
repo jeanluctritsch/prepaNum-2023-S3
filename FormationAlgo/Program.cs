76c5271a8cac2ce0142fa1cf7fadac63a569f46d
Jean-Luc Tritsch <jeanluc.tritsch@jlt-devconsulting.fr>
2023-11-21 11:49:17 +0100

    Solution avec TryParse

--- FormationAlgo/Program.cs
+++ FormationAlgo/Program.cs
@@ -1,7 +1,14 @@
 ﻿// Demander l'age de l'utilisateur
 Console.Write("Veuillez saisir votre âge : ");
 string saisieAge = Console.ReadLine();
-int age = int.Parse(saisieAge);
+bool isAgeSuccess = int.TryParse(saisieAge, out int age);
+
+if (isAgeSuccess == false)
+{
+    Console.Write("Veuillez saisir votre âge : ");
+    saisieAge = Console.ReadLine();
+    isAgeSuccess = int.TryParse(saisieAge, out age);
+}
 
 // Lui demander s'il est une fille ou un garçon 
 // 1. Fille
@@ -15,20 +22,38 @@ Console.WriteLine("2. Garçon");
 Console.WriteLine();
 Console.Write("Sélectionnez 1 ou 2 : ");
 string saisieGenre = Console.ReadLine();
-int choixGenre = int.Parse(saisieGenre);
+bool isGenreSuccess = int.TryParse(saisieGenre, out int choixGenre);
 
-//A partir de ces informations, afficher le texte :
-//Tu es mineur(e)
-//ou
-//Tu es majeur(e)
-string messageFinal = "Tu es ";
+if (isGenreSuccess == false)
+{
+    Console.WriteLine("Êtes-vous une fille ou un garçon ?");
+    Console.WriteLine("1. Fille");
+    Console.WriteLine("2. Garçon");
+    Console.WriteLine();
+    Console.Write("Sélectionnez 1 ou 2 : ");
+    saisieGenre = Console.ReadLine();
+    isGenreSuccess = int.TryParse(saisieGenre, out choixGenre);
+}
 
-if (age < 18)
-    messageFinal += "mineur"; //messageFinal = messageFinal + "mineure.";
+if (isAgeSuccess && isGenreSuccess)
+{
+    //A partir de ces informations, afficher le texte :
+    //Tu es mineur(e)
+    //ou
+    //Tu es majeur(e)
+    string messageFinal = "Tu es ";
+
+    if (age < 18)
+        messageFinal += "mineur"; //messageFinal = messageFinal + "mineure.";
+    else
+        messageFinal += "majeur";
+
+    if (choixGenre == 1)
+        messageFinal += "e";
+
+    Console.WriteLine(messageFinal);
+}
 else
-    messageFinal += "majeur";
-
-if (choixGenre == 1)
-    messageFinal += "e";
-
-Console.WriteLine(messageFinal);
+{
+    Console.WriteLine("Problème malgré 2 tentatives !");
+}
