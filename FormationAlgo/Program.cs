8d702564b914c1740f7d0fa27fcdd59d83ba948a
Jean-Luc Tritsch <jeanluc.tritsch@jlt-devconsulting.fr>
2023-11-28 10:27:52 +0100

    Utilisation boucle do... while

--- FormationAlgo/Program.cs
+++ FormationAlgo/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿bool nombreSaisiEstInvalide = false;
+
+do
+{
+    // Receuillir la saisie du nombre de l'utilisateur
+    Console.WriteLine("Veuillez saisir un nombre entre 10 et 20 (compris) :");
+    string saisie = Console.ReadLine();
+    int nombreSaisi = int.Parse(saisie);
+    nombreSaisiEstInvalide = nombreSaisi < 10 || nombreSaisi > 20;
+    // En fonction de la saisie, afficher "Trop grand" ou "Trop petit"
+    if (nombreSaisi < 10)
+    {
+        Console.WriteLine("Trop petit !");
+    }
+    else if (nombreSaisi > 20)
+    {
+        Console.WriteLine("Trop grand !");
+    }
+}
+while (nombreSaisiEstInvalide);
+
+Console.WriteLine("C'est bon, tu peux passer");
