f1090ba225f7ce10d919d9053e77456ce2d9a1e3
Jean-Luc Tritsch <jeanluc.tritsch@jlt-devconsulting.fr>
2023-11-28 11:49:38 +0100

    Différentes solutions

--- FormationAlgo/Program.cs
+++ FormationAlgo/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿// On veut le tableau suivant : [0, 1, 2, 3, 4, 5]
+
+// Solution ultra rapide :
+int[] tableau = new int[] { 0, 1, 2, 3, 4, 5 };
+
+
+
+// Solution plus évolutive sans boucle for :
+int[] tableau1 = new int[6];
+
+tableau1[0] = 0;
+tableau1[1] = 1;
+tableau1[2] = 2;
+tableau1[3] = 3;
+tableau1[4] = 4;
+tableau1[5] = 5;
+
+// Solution plus évolutive avec boucle for :
+int[] tableau2 = new int[6];
+
+for (int i = 0; i < 6; i++)
+{
+    tableau2[i] = i;
+}
+
